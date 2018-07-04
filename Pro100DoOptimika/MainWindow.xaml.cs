b5cd1da05cb63ba4fa219fb80d623f2a29c20471
p-banach <patryk.banach21@gmail.com>
2018-07-04 08:16:20 +0200

    init

---
+++ Pro100DoOptimika/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace Pro100DoOptimika
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.pathToFile.Text = ofd.FileName;
+            }
+        }
+
+        public void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            if (this.pathToFile.Text == "")
+            {
+                System.Windows.MessageBox.Show("Nie wybrano pliku do konwersji.");
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Rozpoczynam konwersję.");
+                Conversion conversion = new Conversion();
+                FileContentsBox.Text = pathToFile.Text;
+
+                conversion.StartConversion(pathToFile.Text);
+                FileContentsBox.Text = string.Join("\n", conversion.FileContents);
+            }
+        }
+
+
+    }
+
+
+}
---
+++ Pro100DoOptimika/src/Conversion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Pro100DoOptimika
+{
+    class Conversion
+    {
+
+        public Conversion()
+        {
+        }
+
+        public string[] FileContents { get; set; }
+
+
+        public void StartConversion(string pathToFile)
+        {
+            FileContents = System.IO.File.ReadAllLines(pathToFile, Encoding.GetEncoding(1250));
+        }
+    }
+}
